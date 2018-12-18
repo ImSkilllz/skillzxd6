d4166f33db9cac7e6e3a7390d1ac5860caf1900c
ImSkilllz <43930333+ImSkilllz@users.noreply.github.com>
2018-12-18 11:11:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
-  const Discord = require('discord.js');
+ const Discord = require('discord.js');
   const bot1 = new Discord.Client();
   bot1.on("ready", async () => {
   console.log(`Username: ${bot1.user.tag}`);
   console.log(`on: ${bot1.guilds.size} guild(s)`);
-  let guildid = '523637201825955842',
-      cid = '523637480986116102';
+  let guildid = '524344794533724181',
+      cid = '524345196671139840';
   let guild = bot1.guilds.get(guildid);
   let channel = guild.channels.find(c => c.id == cid);
   let size = 0;
@@ -22,8 +22,8 @@
 
 bot1.on('message',function(message) {
 let args = message.content.split(" ").slice(1).join(" ");
-if(message.content.startsWith("1say")) {
-if(message.author.id !== '475297936394289162') return;
+if(message.content.startsWith("16say")) {
+if(message.author.id !== '523969994212638741') return;
 if(!args) return;
 message.channel.send(`${args}`);
 }
@@ -35,8 +35,8 @@ const bot2 = new Discord.Client();
 bot2.on("ready", async () => {
   console.log(`Username: ${bot2.user.tag}`);
   console.log(`on: ${bot2.guilds.size} guild(s)`);
-  let guildid = '523637201825955842',
-      cid = '523637480986116102';
+  let guildid = '524344794533724181',
+      cid = '524345196671139840';
   let guild = bot2.guilds.get(guildid);
   let channel = guild.channels.find(c => c.id == cid);
   let size = 0;
@@ -54,8 +54,8 @@ bot2.on("ready", async () => {
 
 bot2.on('message',function(message) {
 let args = message.content.split(" ").slice(1).join(" ");
-if(message.content.startsWith("2say")) {
-if(message.author.id !== '475297936394289162') return;
+if(message.content.startsWith("17say")) {
+if(message.author.id !== '523969994212638741') return;
 if(!args) return;
 message.channel.send(`${args}`);
 }
@@ -67,8 +67,8 @@ const bot3 = new Discord.Client();
 bot3.on("ready", async () => {
   console.log(`Username: ${bot3.user.tag}`);
   console.log(`on: ${bot3.guilds.size} guild(s)`);
-  let guildid = '523637201825955842',
-      cid = '523637480986116102';
+  let guildid = '524344794533724181',
+      cid = '524345196671139840';
   let guild = bot3.guilds.get(guildid);
   let channel = guild.channels.find(c => c.id == cid);
   let size = 0;
@@ -86,8 +86,8 @@ bot3.on("ready", async () => {
 
 bot3.on('message',function(message) {
 let args = message.content.split(" ").slice(1).join(" ");
-if(message.content.startsWith("3say")) {
-if(message.author.id !== '475297936394289162') return;
+if(message.content.startsWith("18say")) {
+if(message.author.id !== '523969994212638741') return;
 if(!args) return;
 message.channel.send(`${args}`);
 }
